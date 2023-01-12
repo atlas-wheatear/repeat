27248ba67726ec898de7e11b82c5b27911f193b1
Edward Moulsdale <eddie.moulsdale@gmail.com>
2023-01-12 19:07:24 +0000

    more documentation

---
+++ example_a.py
@@ -0,0 +1,15 @@
+import string
+from repeat import repeat
+
+@repeat(
+    string.ascii_letters + string.digits + '-_}',
+    30
+)
+def get_flag(candidate_string: str) -> bool:
+    FLAG = 'HTB{fl4gs-4r3_fun}'
+    # this is a silly example
+    return FLAG.startswith(candidate_string)
+
+print(
+    get_flag(initial='HTB{')
+)
