0c95d29c49dd8f351b2f9ea40a7e5410fd5c911f
Edward Moulsdale <eddie.moulsdale@gmail.com>
2023-01-14 19:50:51 +0000

    add test

--- repeat/test/test_repeat.py
+++ repeat/test/test_repeat.py
@@ -11,3 +11,12 @@ def test_basic_max_length():
 
     match = test_function()
     assert match == 'lol'
+
+
+def test_basic_max_length_parallel():
+    @repeat(string.ascii_lowercase, 3)
+    def test_function(candiate_string: str) -> bool:
+        return "lol".startswith(candiate_string)
+
+    match = test_function(parallelism=5)
+    assert match == 'lol'
