566862483b907fddeae663f6b71f31933ac6ac36
Edward Moulsdale <eddie.moulsdale@gmail.com>
2023-04-11 16:28:50 +0100

    add new test, fix spelling

--- test/test_repeat.py
+++ test/test_repeat.py
@@ -2,12 +2,13 @@ import pytest, string
 
 
 from wheatear_repeat import repeat
+from wheatear_repeat.repeat import MaxLengthReachedException
 
 
 def test_basic_max_length():
     @repeat
-    def test_function(candiate_string: str) -> bool:
-        return "lol".startswith(candiate_string)
+    def test_function(candidate_string: str) -> bool:
+        return "lol".startswith(candidate_string)
 
     test_function.characters = string.ascii_lowercase
     test_function.max_length = 3
@@ -17,11 +18,24 @@ def test_basic_max_length():
 
 def test_basic_max_length_parallel():
     @repeat
-    def test_function(candiate_string: str) -> bool:
-        return "lol".startswith(candiate_string)
+    def test_function(candidate_string: str) -> bool:
+        return "lol".startswith(candidate_string)
 
     test_function.characters = string.ascii_lowercase
     test_function.max_length = 3
     test_function.parallelism = 5
     match = test_function()
     assert match == 'lol'
+
+
+def test_max_length_exceeded():
+    @repeat
+    def test_function(candidate_string: str) -> bool:
+        return False
+
+    test_function.characters = string.ascii_lowercase
+    test_function.max_length = 3
+    test_function.parallelism = 5
+
+    with pytest.raises(MaxLengthReachedException):
+        test_function()
